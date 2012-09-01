b64a2259ea286440d33f129cb4e5b2d1ac82b160
Victor Hugo Marques <87victorhugo@gmail.com>
2012-09-01 07:38:35 -0300

    Fix Merge

--- CrushMe.Api/Models/UserData/NewUserApiModel.cs
+++ CrushMe.Api/Models/UserData/NewUserApiModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace CrushMe.Api.Model.UserData
+namespace CrushMe.Api.Models.UserData
 {
     public class NewUserApiModel
     {
