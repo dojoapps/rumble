579f8a3f88f22f25e6abe79863425cff612e7f69
Victor Hugo Marques <87victorhugo@gmail.com>
2012-09-01 07:37:12 -0300

    fix merge basecontroller

--- CrushMe.Api/Controllers/BaseApiController.cs
+++ CrushMe.Api/Controllers/BaseApiController.cs
@@ -16,9 +16,8 @@ namespace CrushMe.Api.Controllers
         public User CurrentUser { get; set; }
         public FacebookClient FbClient { get; private set; }
 
-        protected override void Initialize(System.Web.Http.Controllers.HttpControllerContext controllerContext)
+        public BaseApiController()
         {
-<<<<<<< HEAD
             FbClient = new FacebookClient();
         }
 
@@ -27,23 +26,12 @@ namespace CrushMe.Api.Controllers
             base.Initialize(controllerContext);
 
             if (Request.Headers.Contains(Constants.FbTokenHeader))
-=======
-            base.Initialize(controllerContext);
-        
-            if(Request.Headers.Contains(Constants.FbTokenHeader))
->>>>>>> feature/userApiAccess
             {
                 FbClient.AccessToken = Request.Headers.GetValues(Constants.FbTokenHeader).First().ToString();
                 dynamic me = FbClient.Get("me");
 
-<<<<<<< HEAD
                 long id = long.Parse(me.id);
                 CurrentUser = db.Users.Find(id);
-=======
-                int id = me.id;
-                if(db.Users.Any(user => user.FbId == id))
-                    CurrentUser = db.Users.Find(id);
->>>>>>> feature/userApiAccess
             }
         }
     }
