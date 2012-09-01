74622aafa089cf2bcaa7155cc5209d1ea3795c8c
Vicente de Alencar <vicentedealencar@gmail.com>
2012-09-01 04:42:02 -0300

    entity

---
+++ CrushMe.Database/CrushMeContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using CrushMe.Database.Models;
+
+namespace CrushMe.Database
+{
+    public class CrushMeContext : DbContext
+    {
+        public CrushMeContext()
+        {
+            System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CrushMeContext>());
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Crush> Crushes { get; set; }
+        public DbSet<CrushOption> CrushOptions { get; set; }
+    }
+}
---
+++ CrushMe.Database/Models/Crush.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace CrushMe.Database.Models
+{
+    public class Crush
+    {
+        public int Id { get; set; }
+
+        public virtual User Crusher { get; set; }
+        public int? CrusherId { get; set; }
+
+        public virtual User Target { get; set; }
+        public int? TargetId { get; set; }
+
+        public virtual List<CrushOption> Options { get; set; }
+
+        public int FatherCrushId { get; set; }
+        public virtual Crush FatherCrush { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateReplied { get; set; }
+
+        public DateTime DateExpires { get; set; }
+    }
+}
